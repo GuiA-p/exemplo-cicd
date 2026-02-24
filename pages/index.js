5ae10ab000aeb9f32a0bfb5ab6c37d5565ce9bdb
Guilherme Abatemarco <guiabatemarco@hotmail.com>
2026-02-24 17:07:05 -0300

    Update criando preview

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import NextLink from "next/link";
 export default function HomeScreen() {
     return(
         <div>
-            <h1>Pagina Inicial</h1>
+            <h1>Pagina Inicial [Criando Preview]</h1>
             <NextLink href='/sobre'>
                 Sobre
             </NextLink>
